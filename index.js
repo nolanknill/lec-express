39e8cac3c3cfa4f878c49a43f7745bd1877fc73d
Nolan Knill <nolanknill@gmail.com>
2022-11-15 12:05:44 -0500

    Add static files and cors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 8080;
 const contestantsRoutes = require("./routes/contestants");
 const requireApiKey = require("./middleware/requireApiKey");
 
-app.use(express.json());
+// TODO: figure out not calling middleware when static assets are loaded
+// TODO: paths in route files
+
+app.use(cors());
+app.use(express.static("public"));
 app.use(requireApiKey);
+app.use(express.json());
 
 app.use("/contestants", contestantsRoutes);
 
